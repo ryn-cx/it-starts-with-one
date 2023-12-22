d80fd18fc0723d7e4ce0df29fde8d2d49425845a
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-22 01:59:10 -0800

    Improve logging and ruff configuration

--- python/create.sh
+++ python/create.sh
@@ -5,7 +5,14 @@ read -p "Project Name: " project_name
 read -p "Project Description: " project_description
 read -p "Project Path: " project_path
 
+# If project_path is empty make the project in the output folder
+# This will cause the venv to be invalid, but everything else will work
+if [ -z "$project_path" ]; then
+    project_path="output/$project_name"
+fi
+
 # Display information and ask for confirmation
+echo "\nCreating project..."
 echo "Project Name: $project_name"
 echo "Project Description: $project_description"
 echo "Project Path: $project_path"
@@ -42,6 +49,23 @@ source .venv/bin/activate
 pip install poetry
 poetry init --quiet --name "$project_name" --license "GNU GPLv3" --description "$project_description"
 poetry add --group dev ruff pre-commit pylint pytest
+
+echo "
+[tool.ruff]
+line-length = 120
+
+[tool.ruff.lint]
+select = ["ALL"]
+ignore = ["ANN101", "ANN102", "N804"]
+# ANN101 - missing-type-self - self doesn't need type hints because the type is known implicitly
+# ANN102 - missing-type-cls - cls doesn't need type hints because the type is known implicitly
+# N804 - non-pep604-annotation - Sometimes has false positives, and Pylance is more accurate
+
+[tool.ruff.extend-per-file-ignores]
+"test_*.py" = ["S101", "INP001"]
+# S101 - assert - Assert statements are fine in tests
+# INP001 - implicit-namespace-package - Tests are not packages and should not have __init__.py files
+" >>pyproject.toml
 
 # Install pre-commit hooks
 pre-commit install
