603b4dfaf0e30795a2d29bb402ff7650bd9380d2
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-21 21:08:19 -0800

    Manage path and description as well

--- README.md
+++ README.md
@@ -4,7 +4,4 @@ Simple scripts for creating new projects with consistent configurations.
 
 ## Usage
 
-Run `create.sh` with your project name as an argument:
-
-```shellscript
-./create.sh project_name
+Run `create.sh` and follow the directions
--- python/create.sh
+++ python/create.sh
@@ -1,21 +1,37 @@
 set -e              # Exit on error
 trap deactivate ERR # Deactivate venv on error
 
-project_name=$1
+read -p "Project Name: " project_name
+read -p "Project Description: " project_description
+read -p "Project Path: " project_path
 
-if [ -z "$project_name" ]; then
-    echo "No project name provided. Usage: ./create.sh <project_name>"
+# Display information and ask for confirmation
+echo "Project Name: $project_name"
+echo "Project Description: $project_description"
+echo "Project Path: $project_path"
+
+read -p "Is this correct? [y/N] " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Aborting."
     exit 1
 fi
 
-# Check if directory exists and delete it
-if [ -d "output/$project_name" ]; then
-    rm -rf "output/$project_name"
+# Library for working with files and file paths
+# /Users/ybr/Developer/common/paved-path
+
+# Remove trailing slash if present
+project_path=${project_path%/}
+
+# If the project already exists rename it
+if [ -d "$project_path" ]; then
+    timestamp=$(date +%Y%m%d%H%M%S)
+    mv "$project_path" "${project_path}_$timestamp"
 fi
 
-mkdir -p "output/$project_name"
-cp -a files/. "output/$project_name"
-cd "output/$project_name"
+mkdir -p "$project_path"
+cp -a files/. "$project_path"
+cd "$project_path"
 git init
 
 # Create and activate the venv
@@ -24,10 +40,14 @@ source .venv/bin/activate
 
 # Install and configure Poetry
 pip install poetry
-poetry init --quiet --name "$project_name" --license "GNU GPLv3"
+poetry init --quiet --name "$project_name" --license "GNU GPLv3" --description "$project_description"
 poetry add --group dev ruff pre-commit pylint flake8 pytest
 
 # Install pre-commit hooks
 pre-commit install
+
+# Create a simple readme file
+echo "# $project_name" >README.md
+echo "$project_description" >>README.md
 
 echo "Setup completed."
