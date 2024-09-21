447874c27ed3ac2c2504abd26ad287f80d7529fd
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2024-09-21 02:54:05 -0700

    initial version

---
+++ python/root_to_path.sh
@@ -0,0 +1,8 @@
+# Dynamically find the python folder in thr .venv folder
+folder_path=$(realpath .venv/lib/python*)
+
+# Get the path for root.pth which will be created to modify the PYTHONPATH
+root_path="$folder_path/site-packages/root.pth"
+
+# Add the root folder of the project to root_path so importing is simplified
+echo "../../../.." > $root_path
